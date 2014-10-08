508cfff360c02902244baf845d875eac116b4885
Vince <vincent.malley@insalien.org>
2014-10-08 16:00:48 -0400

    add title for settings activity

--- app/src/main/java/blogr/vpm/fr/blogr/activity/PreferenceActivity.java
+++ app/src/main/java/blogr/vpm/fr/blogr/activity/PreferenceActivity.java
@@ -11,6 +11,7 @@ public class PreferenceActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("Blog Settings");
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new BlogPreferenceFragment())
                 .commit();
